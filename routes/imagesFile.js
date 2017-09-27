7a51bc6c86428c72d08d7985c9abbde1b4116547
Drumil Bakhai <drums.b123@gmail.com>
2017-09-27 19:13:20 -0400

    Made changes to the controllers and server files. Able to read the filename to be uploaded. Also file upload is possible. Next is to store on the mongodb about the file details

--- routes/imagesFile.js
+++ routes/imagesFile.js
@@ -3,6 +3,43 @@ var router = express.Router();
 var multer = require('multer');
 var mongoose = require('mongoose');
 
+// var storage = multer.diskStorage({
+// 	destination:'/uploads/',
+// 	filename: function(req,file,callback){
+// 		callback(null,filename.originalname)
+// 	}
+// })
+
+var imageSchema = mongoose.Schema({
+	path:{
+		type: String,
+		required: true
+	},
+	originalname:{
+		type: String,
+		required: true
+	}
+});
+
+var SingleImage = mongoose.model('image', imageSchema);
+
+var upload = multer({ dest: 'uploads/' });
+
 exports.connection = function(req,res){
-	res.send('Application is up and running');
+	// res.send('Application is up and running');
+	res.render('../views/index.ejs');
+};
+
+exports.uploadImage = function(req,res){
+	console.log('file name is '+req.file);
+	if(!req.file){
+		res.send('Please select a file');
+	}
+	else{
+		var imagePath = {}
+		imagePath['path'] = req.file.path;
+		imagePath['originalname'] = req.file.originalname;
+		res.send("File uploaded "+imagePath['originalname']);
+	}
+
 }
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
-
+var multer = require('multer');
 
 var routes = require('./routes/imagesFile');
+var upload = multer({ dest: 'uploads/' })
+
 mongoose.Promise = global.Promise;
-
-
 
 var promise = mongoose.connect('mongodb://localhost/sampleData', {
 	useMongoClient: true
@@ -21,12 +21,14 @@ promise.then(function(db){
 	console.log('Not able to make connection because '+err);
 });
 
-
 app.route('/')
-	  .get(routes.connection);
+	  .get(routes.connection)
+	  .post(upload.single('myimage'),routes.uploadImage)
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+
 
 app.listen(port);
 console.log('Server Started on '+port);
