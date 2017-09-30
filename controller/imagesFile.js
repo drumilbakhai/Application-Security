273cc8db2e9f6b57a2fb29d4484836474658071e
Drumil Bakhai <drums.b123@gmail.com>
2017-09-30 13:54:05 -0400

    Commented the code

--- controller/imagesFile.js
+++ controller/imagesFile.js
@@ -1,3 +1,5 @@
+// The control file to handle different API routes
+
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
@@ -5,6 +7,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 
 mongoose.set('debug', true);
+// Defining mongoose schema.
 var imageSchema = mongoose.Schema({
 	user:{
 		type: String,
@@ -28,6 +31,7 @@ var imageSchema = mongoose.Schema({
 	}
 });
 
+// Setting the mongoose to the different variables
 var SingleImage = mongoose.model('image', imageSchema);
 
 
@@ -35,23 +39,28 @@ exports.connection = function(req,res){
 	res.render('../views/index.ejs');
 };
 
+// The main logic to upload the images to the local directory.
 exports.uploadImage = function(req,res){
 	console.log('file name is '+req.file);
 	if(!req.file){
-		res.send('Please select a file');
+		res.render('../views/error_upload.ejs');
 	}
 	else{
+		// The dictionary imagePath contains different attributes such as path, original name and caption to store in mongoose.
 		var imagePath = {}
 		imagePath['path'] = req.file.path;
 		imagePath['originalname'] = req.file.originalname;
 		imagePath['caption'] = req.body.caption;
+
+		// Calling the helper function to add image to the mongoose. The callback handles the error and data.
 		addImage(imagePath, function(err,data){
 			if(err){
-				res.send('Problem in uploading File');
+				// Calling the page to render error message page.
+				res.render('../views/error_upload.ejs');
 			}
 			else{
-				// TODO: Add a view file to display success message
-				res.send("File uploaded "+imagePath['originalname']);
+				// Calling the page to render Success message page.
+				res.render('../views/success_upload.ejs')	
 			}
 		})
 		
@@ -59,22 +68,12 @@ exports.uploadImage = function(req,res){
 
 }
 
-function addImage(imagePath, callback){
-	SingleImage.create(imagePath, function(err,data){
-		if(err){
-			callback(err,null);
-		}
-		else{
-			callback(null,data);
-		}
-	})
-}
-
 exports.getAllImages = function(req,res){
 	customPagination(req,res);
 }
 
 exports.getImagesByUser = function(req,res){
+	// Call the function customPagination to retreive images specific to the user.
 	var user = req.params.user;
 	customPagination(req,res,user);
 	
@@ -88,6 +87,18 @@ exports.displayPagination= function(req, res){
 	customPagination(req,res);
 }
 
+function addImage(imagePath, callback){
+	SingleImage.create(imagePath, function(err,data){
+		if(err){
+			callback(err,null);
+		}
+		else{
+			callback(null,data);
+		}
+	})
+}
+
+
 function customPagination(req,res,user){
 	var query = {};
 	if(!user){
@@ -96,48 +107,53 @@ function customPagination(req,res,user){
 	else{
 		query['user'] = user
 	}
-	
+	//set default variables
 	var pageSize = 10,
 	    currentPage = 1,
 		images = [],
 		imagesArrays = [],
 		imagesList = [];
-    //set default variables
-
+    
+	// Getting the total number of images from the mongodb
    	SingleImage.count({}, function(err,count){
    		if(err){
    			console.log('err')
    		}
    		else{
    			var totalImages = count;
+   			// pageCount Variables defines the total number of images.
    			var pageCount = Math.ceil(totalImages / pageSize);
    			console.log('Total Images are '+totalImages)
    			console.log("Query is "+query);
+   			// Getting the records of the images using the find method. If the user is mentioned then retrieves the images uploaded only by that user.
    			SingleImage.find(query, ["-_id","path","user","caption"]).sort({'created':-1}).exec(function(err,data){
    				if(err){
    					console.log('Problem in retrieving Images '+err)
    				}
    				else{
    					// console.log(data);
+   					// Making the images array to store the images record from the mongodb.
    					data.forEach(function(record){
    						var results = {};
    						results['user'] = record.user;
 						results['caption'] = record.caption;
 						var finalPath = record.path.replace('public','');
-						results['path'] = finalPath;
-						// finalResults.push(results);	
+						results['path'] = finalPath;	
    						images.push(results);
 
    					})
-   					// res.send(images);
+   					// Here splicing the images array to according to the page size. I.E store the images of 1-10 in index 1 position.
+   					// images 11-20 to index 2 position.
    					while(images.length > 0){
    						imagesArrays.push(images.splice(0,pageSize))
    					}
    					console.log('Images Aray splicer '+imagesArrays);
+   					// Get the data only according the page index. 
+
    					if (typeof req.query.page !== 'undefined') {
 				        currentPage = +req.query.page;
 				    }
-
+				    // If the pagenumber 2 is request all the imagesArray in the index 2 is retreived and send to the pagination front end
    					imagesList = imagesArrays[+currentPage -1];
    					// res.send(imagesList);
    					res.render('../views/pagination.ejs',{
@@ -156,59 +172,3 @@ function customPagination(req,res,user){
    	})
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-   /*   var pageCount = totalImages/8,
-        
-        students = [],
-        studentsArrays = [], 
-        studentsList = [];
-
-    //genreate list of students
-    for (var i = 1; i < totalImages; i++) {
-        students.push({name: 'Student Number ' + i});
-    }
-
-    //split list into groups
-    while (students.length > 0) {
-        studentsArrays.push(students.splice(0, pageSize));
-    }
-
-    //set current page if specifed as get variable (eg: /?page=2)
-    if (typeof req.query.page !== 'undefined') {
-        currentPage = +req.query.page;
-    }
-
-    //show list of students from group
-    studentsList = studentsArrays[+currentPage - 1];
-
-    //render index.ejs view file
-    res.render('../views/pagination.ejs', {
-        students: studentsList,
-        pageSize: pageSize,
-        totalImages: totalImages,
-        pageCount: pageCount,
-        currentPage: currentPage
-    }); */
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+// The main server file that helps to execute different functionality of the web-app.
+
+// Including various dependencies for the executing the code
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+
+// Multer is a express library that allows the user to upload files to the Nodejs Server 
 var multer = require('multer');
 
-var routes = require('./routes/imagesFile');
+// The Controller file for the the logic of uploading and displaying images
+var routesControls = require('./controller/imagesFile');
 
+// The storage variable is used to store the filename on the directory names /public/uploads.
 var storage = multer.diskStorage({
  destination: function(req, file, cb) {
  cb(null, 'public/uploads')
@@ -16,15 +23,19 @@ var storage = multer.diskStorage({
  }
 });
 
+// Setting the multer storage to the variable upload
 var upload = multer({ storage:storage });
+
 
 app.use(express.static(__dirname + '/public'));
 mongoose.Promise = global.Promise;
 
+// Connection to the mongoDB using the mongoose interface.
 var promise = mongoose.connect('mongodb://localhost/sampleData', {
 	useMongoClient: true
 })
 
+// If success then display the success on the console
 promise.then(function(db){
 	console.log('Connection Successfull');
 })
@@ -32,24 +43,24 @@ promise.then(function(db){
 	console.log('Not able to make connection because '+err);
 });
 
+// Different routes to handle the logic. 
 app.route('/')
-	  .get(routes.connection)
-	  .post(upload.single('myimage'),routes.uploadImage)
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+  	.get(routesControls.connection)
+  	.post(upload.single('myimage'),routesControls.uploadImage)
 
 app.route('/images')
-	.get(routes.getAllImages);
+	.get(routesControls.getAllImages);
 
 app.route('/images/:user')
-	.get(routes.getImagesByUser);
+	.get(routesControls.getImagesByUser);
 
 app.route('/redirect')
-   .get(routes.redirect);
+   .get(routesControls.redirect);
 
 app.route('/pagination')
-   .get(routes.displayPagination);
+   .get(routesControls.displayPagination);
+
 
 app.listen(port);
+
 console.log('Server Started on '+port);
